afa13b2a93f3eeb949555c7b1dff340ee0c55cca
alejandrotsx <alejandrosunuc@gmail.com>
2025-02-24 17:35:30 -0600

    fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,13 @@ import path from 'path';
 import fileRoutes from './routes/file.routes';
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://document-parser-frontend.vercel.app", // Permite solo este dominio
+    methods: "GET,POST", // Métodos permitidos
+    allowedHeaders: "Content-Type", // Cabeceras permitidas
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
